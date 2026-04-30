a2efbd305317884370f6856ae64ed89d4dda8d36
Alexandre Lindsey <joueur.galactic@gmail.com>
2026-04-30 21:56:06 +0200

    Table editing

---
+++ www/edit/song.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>List of Clients</title>
+  </head>
+  <body>
+    <style>
+      table {
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+      }
+
+      td, th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+      }
+
+      tr:nth-child(even) {
+        background-color: #dddddd;
+      }
+    </style>
+    <?php
+      echo "\n";
+      $bdd = new PDO('mysql:host=db;dbname=group17;charset=utf8', 'group17', '1234');
+
+      if (isset($_POST['delete'])) {
+        $sql_delete = 'DELETE FROM `Song` WHERE `cd_number` = :cd_number AND `track_number` = :track_number';
+        $params_delete[':cd_number']    = $_POST['cd'];
+        $params_delete[':track_number'] = $_POST['song'];
+        $req_delete = $bdd->prepare($sql_delete);
+        $req_delete->execute($params_delete);
+      }
+
+      if (isset($_POST['save_edit'])) {
+        $sql_save_edit = 'UPDATE `Song` SET `title` = :title, `artist` = :artist, `duration` = :duration, `genre` = :genre WHERE `cd_number` = :cd_number and `track_number` = :track_number';
+        $params_save_edit[':cd_number']    = $_POST['cd'];
+        $params_save_edit[':track_number'] = $_POST['song'];
+        $params_save_edit[':title']        = $_POST['title'];
+        $params_save_edit[':artist']       = $_POST['artist'];
+        $params_save_edit[':duration']     = $_POST['duration'];
+        $params_save_edit[':genre']        = $_POST['song_genre'];
+        $req_save_edit = $bdd->prepare($sql_save_edit);
+        $req_save_edit->execute($params_save_edit);
+      }
+
+      if (isset($_POST['save_add'])) {
+        $sql_track = 'SELECT * FROM `Song`WHERE `cd_number` = :cd_number AND `track_number` = :track_number';
+        $params_track[':cd_number']    = $_POST['cd'];
+        $params_track[':track_number'] = $_POST['track_number'];
+        $req_track = $bdd->prepare($sql_track);
+        $req_track->execute($params_track);
+        $rows_track = $req_track->fetchAll();
+        $amount_track = count($rows_track);
+
+        if ($amount_track > 0) {
+          echo '    <script> alert("A song already has this track number on this CD.") </script>';
+          $_POST['add'] = True;
+        } else {
+          $sql_save_add = 'INSERT INTO `Song` (`cd_number`, `track_number`, `title`, `artist`, `duration`, `genre`) VALUES (:cd_number, :track_number, :title, :artist, :duration, :genre)';
+          $params_save_add[':cd_number']    = $_POST['cd'];
+          $params_save_add[':track_number'] = $_POST['track_number'];
+          $params_save_add[':title']        = $_POST['title'];
+          $params_save_add[':artist']       = $_POST['artist'];
+          $params_save_add[':duration']     = $_POST['duration'];
+          $params_save_add[':genre']        = $_POST['song_genre'];
+          $req_save_add = $bdd->prepare($sql_save_add);
+          $req_save_add->execute($params_save_add);
+        }
+      }
+
+      $sql_cd = 'SELECT * FROM `CD`';
+      $req_cd = $bdd->query($sql_cd);
+      $rows_cd = $req_cd->fetchAll();
+      $amount_cd = count($rows_cd);
+
+      if ($amount_cd > 0) {
+        echo '    <form method="post" action="song.php">' . "\n";
+        echo '      <select name="cd">' . "\n";
+        foreach ($rows_cd as $row) {
+          echo '        <option value="' . $row['cd_number'] . '"';
+          if(isset($_POST['cd']) and $row['cd_number'] == $_POST['cd']) echo ' selected';
+          echo '> ' . $row['producer'] . ' | ' . $row['title'] . ' - ' . $row['year'] . ' (' . $row['copies'] . ') </option>' . "\n";
+        }
+        echo '      </select>' . "\n";
+        echo '      <input type="submit" name="cd_submit" value="Select">' . "\n";
+        echo '    </form>' . "\n";
+
+        if (isset($_POST['cd'])) {
+          $sql_song = 'SELECT * FROM `Song` WHERE `cd_number` = :cd_number';
+          $params_song[':cd_number'] = $_POST['cd'];
+          $req_song = $bdd->prepare($sql_song);
+          $req_song->execute($params_song);
+          $rows_song = $req_song->fetchAll();
+          $amount_song = count($rows_song);
+
+          echo "\n";
+          echo '    <form method="post" action="song.php">' . "\n";
+          if ($amount_song > 0) {
+            echo '      <select name="song">' . "\n";
+            foreach ($rows_song as $row) {
+              echo '        <option value="' . $row['track_number'] . '"';
+              if(isset($_POST['song']) and $row['track_number'] == $_POST['song']) echo ' selected';
+              echo '> ' . $row['artist'] . ' | ' . $row['title'] . ' - ' . $row['genre'] . ' (' . $row['duration'] . ') </option>' . "\n";
+            }
+            echo '      </select>' . "\n";
+            echo '      <input type="submit" name="delete" value="Delete">' . "\n";
+            echo '      <input type="submit" name="edit"   value="Edit">'   . "\n";
+          } else {
+            echo '      The CD currently has no songs assigned to it' . "\n";
+          }
+          echo '      <input type="submit" name="add"    value="Add">' . "\n";
+          echo '      <input type="hidden" name="cd"     value="' . $_POST['cd'] . '">' . "\n";
+          echo '    </form>' . "\n";
+
+          if (isset($_POST['edit']) or isset($_POST['add'])) {
+            $sql_genre = 'SELECT * FROM `Genre`';
+            $req_genre = $bdd->query($sql_genre);
+            $rows_genre = $req_genre->fetchAll();
+            $amount_genre = count($rows_genre);
+          }
+
+          if (isset($_POST['edit'])) {
+            $sql_edit = 'SELECT * FROM `Song` WHERE `cd_number` = :cd_number AND `track_number` = :track_number';
+            $params_edit[':cd_number'] = $_POST['cd'];
+            $params_edit[':track_number'] = $_POST['song'];
+            $req_edit = $bdd->prepare($sql_edit);
+            $req_edit->execute($params_edit);
+            $song = $req_edit->fetch();
+
+            echo '    <form method="post" action="song.php">' . "\n";
+            echo '      <input type="text" name="artist"   value="' . $song['artist']   . '" placeholder="Artist"   maxlength="255">' . "\n";
+            echo '      <input type="text" name="title"    value="' . $song['title']    . '"  placeholder="Title"    maxlength="255">' . "\n";
+            echo '      <input type="time" name="duration" value="' . $song['duration'] . '" placeholder="Duration">'                 . "\n";
+            if ($amount_genre > 0) {
+              echo '      <select name="song_genre">' . "\n";
+              foreach ($rows_genre as $row) {
+                echo '        <option value="' . $row["name"] . '"';
+                if ($row['name'] == $song['genre']) echo ' selected';
+                echo '> ' . $row['name'] . '</option>' . "\n";
+              }
+              echo '      </select>' . "\n";
+            }
+            echo '      <input type="submit" name="save_edit" value="Save">' . "\n";
+            echo '      <input type="hidden" name="cd"   value="' . $_POST['cd']   . '">' . "\n";
+            echo '      <input type="hidden" name="song" value="' . $_POST['song'] . '">' . "\n";
+            echo '    </form>' . "\n";
+          }
+
+          if (isset($_POST['add'])) {
+            echo '    <form method="post" action="song.php">' . "\n";
+            echo '      <input type="number" name="track_number" value="' . trim($_POST['track_number'] ?? '') . '" placeholder="Track number" min="1" step="1">' . "\n";
+            echo '      <input type="text"   name="artist"       value="' . trim($_POST['artist']       ?? '') . '" placeholder="Artist"       maxlength="255">'  . "\n";
+            echo '      <input type="text"   name="title"        value="' . trim($_POST['title']        ?? '') . '" placeholder="Title"        maxlength="255">'  . "\n";
+            echo '      <input type="time"   name="duration"     value="' . trim($_POST['duration']     ?? '') . '" placeholder="Duration">'                      . "\n";
+            if ($amount_genre > 0) {
+              echo '      <select name="song_genre">' . "\n";
+              foreach ($rows_genre as $row) {
+                echo '        <option value="' . $row["name"] . '"';
+                if (isset($_POST['song_genre']) and $row['name'] == $_POST['song_genre']) echo ' selected';
+                echo '> ' . $row['name'] . '</option>' . "\n";
+              }
+              echo '      </select>' . "\n";
+            }
+            echo '      <input type="submit" name="save_add" value="Save">' . "\n";
+            echo '      <input type="hidden" name="cd"   value="' . $_POST['cd']   . '">' . "\n";
+            echo '    </form>' . "\n";
+          }
+        }
+      } else {
+        echo '    There are currently no CDs' . "\n";
+      }
+    ?>
+  </body>
+</html>
